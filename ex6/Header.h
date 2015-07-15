f44106687973c9b3a82bda96102e54f2d1bee5f7
Matsuo Haruka <springflower.kusky@icloud.com>
2015-07-15 17:21:09 +0900

    AFNetworkingライブラリの追加

---
+++ ex6/Header.h
@@ -0,0 +1,16 @@
+//
+//  Header.h
+//  ex6
+//
+//  Created by HARUKA on 7/15/15.
+//  Copyright (c) 2015 HARUKA. All rights reserved.
+//
+
+#ifndef ex6_Header_h
+#define ex6_Header_h
+
+#import <AFNetworking/AFNetworking.h>
+
+#import <AFNetworking/UIImageView+AFNetworking.f>
+
+#endif
