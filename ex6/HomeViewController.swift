93670712bf160c7c4e05e328c717ad51d6e0c516
Matsuo Haruka <springflower.kusky@icloud.com>
2015-08-05 18:11:48 +0900

    HomeViewController,NextViewControllerの追加

---
+++ ex6/HomeViewController.swift
@@ -0,0 +1,40 @@
+//
+//  HomeViewController.swift
+//  ex6
+//
+//  Created by HARUKA on 8/5/15.
+//  Copyright (c) 2015 HARUKA. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBAction func myButton1(sender: UIButton) {
+    }
+    @IBAction func myButton2(sender: AnyObject) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ex6/NextViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NextViewController.swift
+//  ex6
+//
+//  Created by HARUKA on 8/5/15.
+//  Copyright (c) 2015 HARUKA. All rights reserved.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
